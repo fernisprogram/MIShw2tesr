b1ef625249f31f84ffa7febe46c07674154f996d
fernisprogram <floresfernanda695@gmail.com>
2026-07-06 00:47:02 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,22 +5,25 @@ Date Updated:
 Version:2
 Description:MIS Homework Assignment 3 requiring a registration form */
 
-    function validateFirstName(fname) {
-      const firstName = document.getElementById('fname').value;
-      const regex = /^[A-Za-z'-]{1,30}$/;
-      }
-      if (regex.test(firstName)) 
-        alert('First name is valid!');
-       { else 
-        window.alert('Please enter a valid first name (1 to 30 characters, letters, apostrophes, and dashes only).');
-      }
-    
-    function validateLastName(lname) {
-      const lastname= document.getElementById('lname').value;
-      const regex = /^[A-Za-z'-]{1,30}$/;
-      if (regex.test(lname)) {
-        window.alert('Last name is valid');{
-          else }
-          alert ('Please enter a valid Last Name).');
-      }
-      }
+{{javascript}}
+function validateFirstName() {
+  const firstName = document.getElementById('fname').value;
+  const regex = /^[A-Za-z'-]{1,30}$/;
+  
+  if (regex.test(firstName)) {
+    alert('First name is valid!');
+  } else {
+    window.alert('Please enter a valid first name (1 to 30 characters, letters, apostrophes, and dashes only).');
+  }
+}
+
+function validateLastName() {
+  const lastName = document.getElementById('lname').value;
+  const regex = /^[A-Za-z'-]{1,30}$/;
+  
+  if (regex.test(lastName)) {
+    window.alert('Last name is valid');
+  } else {
+    alert('Please enter a valid Last Name.');
+  }
+}
