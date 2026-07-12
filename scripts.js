77185b11fbf020608e4444a3637c3fe4820cbd62
fernisprogram <floresfernanda695@gmail.com>
2026-07-12 06:53:10 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,5 +3,5 @@ Name File:mis3371homework2.html
 Date Created:6/22/26
 Date Updated:
 Version:2
-Description:MIS Homework Assignment 3 requiring a registration form */
+Description:MIS Homework Assignment  requiring a registration form */
 
